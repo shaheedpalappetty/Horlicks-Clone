8556a027669209c5783ed1d2e3d69a7339f797b7
shaheedpalappetty <129977243+shaheedpalappetty@users.noreply.github.com>
2023-04-13 20:57:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+
+let b = document.getElementById("btn_sub")
+let x = document.getElementById("aud");
+
+let inp =document.getElementById("inp");
+
+
+b.addEventListener("click",
+function btn(){
+
+   x.play()
+
+    alert("summited");
+    console.log("clicked");
+    
+});
